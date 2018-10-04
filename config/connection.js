9604c52cf01dda0faeb59a07656ffe6a342b083d
Tom Dennis <tdennis913@gmail.com>
2018-10-04 15:40:43 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'burgers_db'
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
 module.exports = connection;
